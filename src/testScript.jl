5e39f882cc4ed9599c37aefdc9cea05641192f07
JonnyCBB <jonathan.brooks-bartlett@dtc.ox.ac.uk>
2015-09-17 13:24:47 +0100

    add Bfactor plot

--- src/testScript.jl
+++ src/testScript.jl
@@ -1,10 +1,12 @@
-# include("ReciprocalSpaceUtils.jl")
-# include("ElementDatabase.jl")
-# include("MtzdumpHandling.jl")
-# include("SequenceFileParser.jl")
-# include("UpdateAtomAndRefs.jl")
-# using Gadfly
-# using LsqFit
+include("ReciprocalSpaceUtils.jl")
+include("ElementDatabase.jl")
+include("MtzdumpHandling.jl")
+include("SequenceFileParser.jl")
+include("UpdateAtomAndRefs.jl")
+using Gadfly
+using LsqFit
+using Colors
+using Compose
 
 ######### Inputs ##########
 const xrayEnergy = 12.7 #Set X-ray Energy
@@ -73,16 +75,6 @@ calcElementf0!(elementDict, scatteringAngles, xrayWavelength)
 updateAtomDict!(atomDict, spacegroup)
 f0SqrdDict = calcTotalf0Sqrd(atomDict, scatteringAngles, elementDict)
 updateRefListAndImageArray!(hklList, imageArray, estimateTotalIntensityFromPartialRef)
-
-function calcResbinMeanIntensity!(resbins::Vector{ResBin}, f0SqrdDict::Dict{Float64, Float64}, hklList::Dict{Vector{Int64}, Reflection})
-    for resbin in resbins
-        summedIntensity = 0.0
-        for hkl in resbin.hkls
-            summedIntensity += f0SqrdDict[hklList[hkl].scatteringAngle]
-        end
-        resbin.meanIntensity = summedIntensity/resbin.numOfRef
-    end
-end
 calcResbinMeanIntensity!(resbins, f0SqrdDict, hklList)
 
 bfactors = Vector{Float64}()
@@ -128,6 +120,25 @@ for img in imageArray
 end
 model(x,p) = p[1] + p[2]*x
 fit = curve_fit(model, imageNumArray, bfactors,[0.0,0.0])
-fit.param[2]
+changeInBfac = fit.param[2]
+startAndEndBfac = model([imageNumArray[1], imageNumArray[end]], fit.param)
+midBfac = (startAndEndBfac[1] + startAndEndBfac[end])/2
+
+n = 2
+getColors = distinguishable_colors(n, Color[LCHab(70, 60, 240)],
+               transform=c -> deuteranopic(c, 0.5),
+               lchoices=Float64[65, 70, 75, 80],
+               cchoices=Float64[0, 50, 60, 70],
+               hchoices=linspace(0, 330, 24))
+xvals = linspace(0, length(imageArray), 100)
+yvals = model(xvals, fit.param)
+plotTitle = @sprintf("Line Gradient: %.4f", fit.param[2])
+plot(
+layer(x=xvals, y=yvals, Geom.line, Theme(default_color=getColors[1])),
+layer(x=imageNumArray, y=bfactors, Geom.point, Theme(default_color=getColors[2])),
+Guide.xlabel("Image Number"), Guide.ylabel("B-factor"),
+Guide.manual_color_key("Colour Key",["Line of best fit", "Data"],[getColors[1],getColors[2]]),
+Guide.title(plotTitle)
+)
 #End Section: Update atom and reflection information
 ################################################################################
